88edbfe0c6a2c1749779e335986a4182378117f7
Tyler Larrabee <tyer.k.larrabee@gmail.com>
2019-11-13 23:13:30 -0800

    second commit why server.js no start?

--- initative-tracker/src/App.js
+++ initative-tracker/src/App.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+
 /*TODO
 > Find Game
 > Create Game
@@ -21,6 +26,30 @@ Computer Things
 */ 
 
 
-const createGame = (gameName, password) => {
+// const createGame = (gameName, password) => {
     
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
+
